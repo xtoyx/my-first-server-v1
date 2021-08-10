dcebb49270109e22efd2498acdbed8fdabed1b16
xtoyx <max.pan13@hotmail.com>
2021-08-10 12:14:14 -0400

    ok maybe this will work

--- djhcjfirstwebsite/settings.py
+++ djhcjfirstwebsite/settings.py
@@ -29,7 +29,7 @@ with open(os.path.join(BASE_DIR,'secert_stuff.txt')) as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*',]
+ALLOWED_HOSTS = ['*','notchv1.herokuapp.com']
 
 
 # Application definition
