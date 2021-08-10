999936959bf92cf00ca8d6f43a6bea0f109b1043
xtoyx <max.pan13@hotmail.com>
2021-08-10 16:56:25 -0400

    DO i need to do this

--- djhcjfirstwebsite/settings.py
+++ djhcjfirstwebsite/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR , 'static'),
 ]
-STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
