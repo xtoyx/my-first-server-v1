bcdd498a9efd988e17d1c59b1e2664eb21493e8b
xtoyx <max.pan13@hotmail.com>
2021-08-10 12:32:00 -0400

    okssdasdss

--- djhcjfirstwebsite/settings.py
+++ djhcjfirstwebsite/settings.py
@@ -27,7 +27,7 @@ with open(os.path.join(BASE_DIR,'secert_stuff.txt')) as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
